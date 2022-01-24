bcf9a620273859efacac40549805b25189ef71c2
k9coders <k9coders@gmail.com>
2022-01-24 03:18:34 +0300

    final

---
+++ Exchange app-2/Data.swift
@@ -0,0 +1,16 @@
+//
+//  data.swift
+//  Exchange app-2
+//
+//  Created by Oleg Shum on 22.01.2022.
+//
+
+import Foundation
+
+struct Data: Codable {
+    var base: String
+    var date: String
+    var rates: Rates
+}
+
+
---
+++ Exchange app-2/Rates.swift
@@ -0,0 +1,14 @@
+//
+//  Rates.swift
+//  Exchange app-2
+//
+//  Created by Oleg Shum on 22.01.2022.
+//
+
+import Foundation
+
+struct Rates: Codable {
+    var EUR: Double
+    var USD: Double
+    var GBP: Double
+}
